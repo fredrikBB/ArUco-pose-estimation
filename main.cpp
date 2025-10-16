b72922867e19c4dbe83e4472fc3fbea555715fe6
fredrikBB <fredrik.b.bratberg@gmail.com>
2025-10-16 13:45:04 +0200

    Detect ArUco markers and draw them on input image

--- main.cpp
+++ main.cpp
@@ -7,20 +7,44 @@ int main(int argc, char** argv )
 {
     if ( argc != 2 )
     {
-        printf("usage: DisplayImage.out <Image_Path>\n");
+        printf("usage: ArUcoDetection.out <Image_Path>\n");
         return -1;
     }
 
-    Mat image;
-    image = imread( argv[1], 1 );
+    Mat InputImage;
+    InputImage = imread( argv[1], 1 );
 
-    if ( !image.data )
+    if ( !InputImage.data )
     {
         printf("No image data \n");
         return -1;
     }
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
+
+    // ArUco detection
+    std::vector<int> markerIds;
+    std::vector<std::vector<cv::Point2f>> markerCorners, rejectedCandidates;
+    cv::aruco::DetectorParameters detectorParams = cv::aruco::DetectorParameters();
+    cv::aruco::Dictionary dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_50);
+    cv::aruco::ArucoDetector detector(dictionary, detectorParams);
+    detector.detectMarkers(InputImage, markerCorners, markerIds, rejectedCandidates);
+
+    // Draw results
+    if (markerIds.size() > 0)
+        cv::aruco::drawDetectedMarkers(InputImage, markerCorners, markerIds);
+    else
+        std::cout << "No markers detected" << std::endl;
+
+    // Show image
+    cv::namedWindow("Detected markers", cv::WINDOW_NORMAL);
+    imshow("Detected markers", InputImage);
+
+    // Print out detected marker ids sorted
+    std::sort(markerIds.begin(), markerIds.end());
+    std::cout << "Detected marker ids: ";
+    for (auto i : markerIds)
+        std::cout << i << " ";
+    std::cout << std::endl;
+    std::cout << "Number of detected markers: " << markerIds.size() << std::endl;
 
     waitKey(0);
 
